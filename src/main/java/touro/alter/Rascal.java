b5ef3fd0e8b72a63cbc20d14879503785abb4f6e
shiraalter <shiraalter@gmail.com>
2020-11-28 21:55:27 -0500

    cleaned

--- src/main/java/touro/alter/Rascal.java
+++ src/main/java/touro/alter/Rascal.java
@@ -8,15 +8,13 @@ public class Rascal {
     public int getValue(int row, int col) {
         int halfOfRowNum = (int) Math.round((double) (row / 2));
         int finalValue = 1;
+        int valueBetweenColumn = row - 1;  //gets second index
 
         /*after the middle index, numbers repeat themselves until the end.
         if col index is higher than half the amount of indexes in row, assign it to its 'twin' in first half of row*/
         if (col > halfOfRowNum) {
             col = row - col;
         }
-
-        int valueBetweenColumn = row - 1;  //gets second index
-
         for (int i = 0; i < col; i++) {
             finalValue += valueBetweenColumn;
             valueBetweenColumn -= 2;
